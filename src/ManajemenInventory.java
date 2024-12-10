9f4c91e1d96f868e51bfdf5e23e50eec39ff5464
wandi1209 <wandider@gmail.com>
2024-12-10 11:19:20 +0700

    Kerangka Manajemen Inventory

---
+++ src/ManajemenInventory.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ManajemenInventory {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+
+    static void addData() {
+
+    }
+
+    static void addStock() {
+
+    }
+
+    static void newData() {
+
+    }
+}
